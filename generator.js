132d3c8050ed53ee6925712b23424d56afa8e046
sankalp <sankalp@habilelabs.io>
2020-01-06 17:49:08 +0530

    made project generation interactive

--- generator.js
+++ generator.js
@@ -1,54 +1,60 @@
-const {Command} = require("commander");
+const prompts = require('prompts');
 const fs = require("fs");
 const {execSync} = require('child_process');
 const unpack = require('tar-pack').unpack;
 const path = require('path');
 const request = require('request');
-const { version, name } = require('./package.json');
 
-const program = new Command(name)
-.version(version)
-.arguments('<project-name>')
-.usage(`<project-name> [options]`)
-.action(name => {
-    projectName = name;
-})
-.on("--help", () => {
-    console.log("Only <project-name> is required");
-})
-.option("-p, --port <port>", "port number on which Node app will run")
-.option("-dbName, --database-name <name>", "database name for application")
-.option("-c, --enable-clustering", "whether to enable clustering(through child process) or not.")
-.option("-e, --env <env>", "environment to set for the app. Can be 'development'/'production'")
-.parse(process.argv);
-if (program.env && program.env !== "development" && program.env !== "production") {
-  console.log("Invalid value for argument '--env'");
-  process.exit(1);
-}
+const questions = [
+    { 
+        type: 'text', 
+        name: 'projectName', 
+        message: 'Enter project name',
+        validate: function (value) {
+            if (value.length > 0) {
+                return true;
+            }
+            return "Please enter a valid project name.";
+        }
+    },{ 
+        type: 'text', 
+        name: 'portNumber', 
+        message: 'Enter port number',
+        initial: 8080,
+        validate: function (value) {
+            value = parseInt(value);
+            if (/^[0-9]{1,5}$/.test(value) && value >= 0 && value <= 65535) {
+                return true;
+            }
+            return "Please enter a valid port number.";
+        }
+    },{ 
+        type: 'text', 
+        name: 'dbName', 
+        message: 'Enter database name',
+        validate: function (value) {
+            if (value.length > 0) {
+                return true;
+            }
+            return "Please enter a valid database name.";
+        }
+    },
+    { 
+        type: 'confirm', 
+        name: 'enableClustering', 
+        message: 'Do you want to enable clustering through child_process module?', 
+        default: false 
+    },
+    {
+        type: 'select', 
+        name: 'env', 
+        message: 'Please select environment to set', 
+        choices: ["development", "production"],
+        initial: 0
+    }
+];
 
-if (typeof projectName === 'undefined') {
-    console.error('Please specify the project name:');
-    console.log(
-      `  ${program.name()} <project-name>`
-    );
-    console.log();
-    console.log('For example:');
-    console.log(`  ${program.name()} my-node-app`);
-    console.log();
-    console.log(
-      `Run ${program.name()} --help to see all options.`
-    );
-    process.exit(1);
-}
-
-const root = path.resolve(projectName);
-const appName = path.basename(root);
-const templateToInstall = "node-mongo-starter-kit";
-const templateFormat = "tarball";
-const templateHost = "api.github.com";
-const templateHostUser = "habilelabs";
-
-const checkIfDirExistsOrEmpty = () => {
+const checkIfDirExistsOrEmpty = (root) => {
   try {
     const files = fs.readdirSync(root);
     return { exists: true, isEmpty: files.length === 0 };
@@ -58,7 +64,7 @@ const checkIfDirExistsOrEmpty = () => {
   }
 }
 
-const downloadAndUnPackKit = () => {
+const downloadAndUnPackKit = (root, templateToInstall, templateFormat, templateHost, templateHostUser) => {
   return new Promise((resolve, reject)=>{
     request({
       uri: `https://${templateHost}/repos/${templateHostUser}/${templateToInstall}/${templateFormat}/master`,
@@ -74,8 +80,8 @@ const downloadAndUnPackKit = () => {
   });
 }
 
-const createApp = () => {
-  const dirInfo = checkIfDirExistsOrEmpty();
+const createApp = (root, appName, templateToInstall, templateFormat, templateHost, templateHostUser) => {
+  const dirInfo = checkIfDirExistsOrEmpty(root);
   if (dirInfo.exists && !dirInfo.isEmpty) {
     // directory already exists and is not empty, throw error 
     throw new Error(`Directory with name '${appName}' already exists. Please specify some other project name`);
@@ -85,9 +91,9 @@ const createApp = () => {
     fs.mkdirSync(root); 
   }
   // directory is empty, OK to continue
-  return downloadAndUnPackKit();
+  return downloadAndUnPackKit(root, templateToInstall, templateFormat, templateHost, templateHostUser);
 }
-const getPackageJsonContents = (path) => {
+const getPackageJsonContents = (appName, path) => {
   const packageJson = require(path);
   return {
     name: appName,
@@ -98,38 +104,50 @@ const getPackageJsonContents = (path) => {
     devDependencies: packageJson.devDependencies,
   }
 }
-const installNpmPackages = () => {
+const installNpmPackages = (root) => {
   console.log("Installing dependencies...");
   execSync(`cd ${root} && npm i`, {stdio: 'inherit'});
   console.log("Dependencies installed.");
 }
 
-const addDotEnv = () => {
+const addDotEnv = (root, data) => {
   const envPath = path.join(root, ".env");
-  fs.writeFileSync(envPath, `PORT=${program.port || 8080}
-  ENV="${program.env || "development"}"
-  IS_CLUSTERING_ENABLED=${program.enableClustering || false}
+  fs.writeFileSync(envPath, `PORT=${data.port}
+  ENV="${data.env || "development"}"
+  IS_CLUSTERING_ENABLED=${data.enableClustering}
   SECRET="some secret"
-  MONGO_URI="mongodb://localhost:27017/${program.databaseName || "node-mongo-demo"}"`);
+  MONGO_URI="mongodb://localhost:27017/${data.databaseName}"`);
 }
 
-console.log("Creating app...");
-createApp().then(() => {
-  console.log("Created folder structure.");
-  const pjPath = path.join(root, "package.json");
-  const contents = getPackageJsonContents(pjPath);
-  fs.writeFileSync(pjPath, JSON.stringify(contents, null, 4));
-  addDotEnv();
-  console.log("Created '.env' file");
-  installNpmPackages();
-  console.log("App created.");
-  console.log();
-  console.log(`Start your app by moving into the "${appName}" directory and running
-  npm start`);
-  console.log();
-  console.log("NOTE:- Don't forget to start mongoDB :)")
-  console.log();
-  console.log("Visit https://github.com/habilelabs/node-mongo-starter-kit for more information on this starter kit.");
-}).catch(err => {
-  console.log(err.message);
-});
+(async () => {
+
+    try {
+        const response = await prompts(questions);
+        const root = path.resolve(response.projectName);
+        const appName = path.basename(root);
+        const templateToInstall = "node-mongo-starter-kit";
+        const templateFormat = "tarball";
+        const templateHost = "api.github.com";
+        const templateHostUser = "habilelabs";
+        console.log("Creating app...");
+        await createApp(root, appName, templateToInstall, templateFormat, templateHost, templateHostUser);
+        console.log("Created folder structure.");
+        const pjPath = path.join(root, "package.json");
+        const contents = getPackageJsonContents(appName, pjPath);
+        fs.writeFileSync(pjPath, JSON.stringify(contents, null, 4));
+        addDotEnv(root, response);
+        console.log("Created '.env' file");
+        installNpmPackages(root);
+        console.log("App created.");
+        console.log();
+        console.log(`Start your app by moving into the "${appName}" directory and running
+        npm start`);
+        console.log();
+        console.log("NOTE:- Don't forget to start mongoDB :)")
+        console.log();
+        console.log("Visit https://github.com/habilelabs/node-mongo-starter-kit for more information on this starter kit.");
+        
+    } catch (err) {
+        console.log(err);
+    }
+  })();
